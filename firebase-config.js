93ab0ed1e8e9155e0b4922c681a231c83ce2d825
tidewatertech <84999537+tidewatertech@users.noreply.github.com>
2026-06-09 16:21:13 -0700

    Sign-in fix

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+// Paste your Firebase config values below
+// This file is loaded before app.js and exposes config globally
+
 window.firebaseConfig = {
     apiKey: "AIzaSyCoZu2A7Svq17lAMRArGShlkGoCjT1LzMA",
     authDomain: "mo-mint.firebaseapp.com",
@@ -5,4 +8,4 @@ window.firebaseConfig = {
     storageBucket: "mo-mint.firebasestorage.app",
     messagingSenderId: "597121235085",
     appId: "1:597121235085:web:985a39626d55c4a2d38ac4"
-};
+  };
